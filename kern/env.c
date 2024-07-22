0247e07135054b482080859b8f3e16d464f078b4
Ming-Y-ANG <714536723@qq.com>
2024-07-22 17:41:17 +0330

    lab4 partB
    Signed-off-by: Ming-Y-ANG <714536723@qq.com>

--- kern/env.c
+++ kern/env.c
@@ -532,7 +532,7 @@ env_run(struct Env *e)
 	curenv->env_status = ENV_RUNNING;
 	curenv->env_runs++;
 	lcr3(PADDR(curenv->env_pgdir));
-	cprintf("start env_pop and running...\n");
+	//cprintf("start env_pop and running...\n");
 	unlock_kernel();
 	env_pop_tf(&curenv->env_tf);
 
